a28e4bd8d9f19b167c3fb167bb03c38f30e02ef9
Estevao Coelho <estevaocoelho@Estevaos-iMac.local>
2018-02-15 11:05:36 -0200

    [PART-2] Creating profileActivity and layout

--- app/src/main/java/com/estevaocoelho/contactme/ProfileActivity.java
+++ app/src/main/java/com/estevaocoelho/contactme/ProfileActivity.java
@@ -3,9 +3,10 @@ package com.estevaocoelho.contactme;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by estevaocoelho on 15/02/18.
@@ -20,5 +21,10 @@ public class ProfileActivity extends AppCompatActivity {
         String userName = userSharedPrefs.getString("USER_NAME", "");
         String userImageUrl = userSharedPrefs.getString("USER_IMAGE_URL", "");
 
+        TextView textViewUserName = findViewById(R.id.text_view_user_name);
+        TextView textViewUserPhotoURL = findViewById(R.id.text_view_user_url);
+
+        textViewUserName.setText(userName);
+        textViewUserPhotoURL.setText(userImageUrl);
     }
 }
